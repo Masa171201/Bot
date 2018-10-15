38cd268f4d24232d8c6e021ed9f15ecc4f4b21e8
Masa171201 <obara@chowagiken.co.jp>
2018-10-15 13:16:07 +0900

    First Commit!!

---
+++ main.php
@@ -0,0 +1,27 @@
+﻿<html>
+<head>
+<title>PHP TEST</title>
+</head>
+
+<body>
+
+<?php
+
+$array_shop = array();
+$array_shop[0] = '札幌';
+$array_shop[1] = '旭川';
+$array_shop[2] = '函館';
+$array_shop[3] = '名古屋';
+$array_shop[4] = '松戸';
+$array_shop[5] = '習志野';
+$array_shop[6] = '我孫子';
+
+print_r( $array_shop );
+print('<br>');
+print_r( count( $array_shop ) );
+
+
+?>
+
+</body>
+</html>
