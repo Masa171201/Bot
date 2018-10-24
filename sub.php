cfd1d8dc004554607f68ad9a1e73f40b313510db
Masa171201 <obara@chowagiken.co.jp>
2018-10-25 08:54:08 +0900

    Sub.phpを追加

---
+++ sub.php
@@ -0,0 +1,24 @@
+﻿<html>
+<head>
+<title>PHP TEST</title>
+</head>
+
+<body>
+
+<?php
+
+$array_shop = array();
+$array_shop[0] = '熊本';
+$array_shop[1] = '福岡';
+$array_shop[2] = '鹿児島';
+$array_shop[3] = '長崎33';
+
+print_r( $array_shop );
+print('<br>');
+print_r( count( $array_shop ) );
+
+
+?>
+
+</body>
+</html>
